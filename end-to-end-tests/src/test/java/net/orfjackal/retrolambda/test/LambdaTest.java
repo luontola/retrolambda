f97cd5919715a07f29d0ce21932af571361fd7a2
Esko Luontola <esko.luontola@gmail.com>
2013-07-21 20:23:28 +0300

    Run end-to-end tests using Java 7, but compile with Java 8

---
+++ end-to-end-tests/src/test/java/net/orfjackal/retrolambda/test/LambdaTest.java
@@ -0,0 +1,22 @@
+// Copyright © 2013 Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+package net.orfjackal.retrolambda.test;
+
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class LambdaTest {
+
+    @Test
+    public void lambda_returning_value() throws Exception {
+        Callable<String> lambda = () -> "some value";
+
+        assertThat(lambda.call(), is("some value"));
+    }
+}
