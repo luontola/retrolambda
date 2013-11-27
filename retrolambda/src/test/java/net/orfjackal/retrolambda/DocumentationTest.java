60fc80acce786a1ef9ac217e0ff96c2dcc7a30c6
Esko Luontola <esko.luontola@gmail.com>
2013-11-27 21:36:08 +0200

    Fixed test to work in both IDEA and Maven

--- retrolambda/src/test/java/net/orfjackal/retrolambda/DocumentationTest.java
+++ retrolambda/src/test/java/net/orfjackal/retrolambda/DocumentationTest.java
@@ -6,7 +6,7 @@ package net.orfjackal.retrolambda;
 
 import org.junit.Test;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.Properties;
@@ -16,18 +16,25 @@ import static org.junit.Assert.assertTrue;
 public class DocumentationTest {
 
     @Test
-    public void README_contains_the_usage_instructions() {
-        String readme = toString(Paths.get("README.md"));
+    public void README_contains_the_usage_instructions() throws IOException {
+        String readme = toString(findInClosestParentDir("README.md"));
         String help = new Config(new Properties()).getHelp();
 
         assertTrue("Expected README to contain the following text:\n\n" + help, readme.contains(help));
     }
 
-    private static String toString(Path path) {
-        try {
-            return new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
+    private static Path findInClosestParentDir(String filename) throws IOException {
+        for (Path dir = Paths.get(".").toRealPath(); dir.getParent() != null; dir = dir.getParent()) {
+            Path file = dir.resolve(filename);
+            if (Files.exists(file)) {
+                return file;
+            }
         }
+        throw new FileNotFoundException(filename);
+    }
+
+    private static String toString(Path path) throws IOException {
+        return new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
     }
 }
