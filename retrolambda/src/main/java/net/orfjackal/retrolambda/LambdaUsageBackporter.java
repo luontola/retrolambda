59daeff456dfd2b9929dd52e5f6081a6c1e2d826
Esko Luontola <esko.luontola@gmail.com>
2014-01-07 22:53:51 +0200

    Upgraded to ASM 5.0_BETA, fixes Java 8 workaround for ASM

--- retrolambda/src/main/java/net/orfjackal/retrolambda/LambdaUsageBackporter.java
+++ retrolambda/src/main/java/net/orfjackal/retrolambda/LambdaUsageBackporter.java
@@ -1,4 +1,4 @@
-// Copyright © 2013 Esko Luontola <www.orfjackal.net>
+// Copyright © 2014 Esko Luontola <www.orfjackal.net>
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
@@ -7,7 +7,6 @@ package net.orfjackal.retrolambda;
 import org.objectweb.asm.*;
 
 import java.lang.reflect.Field;
-import java.nio.ByteBuffer;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
 
@@ -15,32 +14,14 @@ import static org.objectweb.asm.Opcodes.*;
 
 public class LambdaUsageBackporter {
 
-    private static final int JAVA_8_BYTECODE_VERSION = 52;
-    private static final int MAJOR_VERSION_OFFSET = 6;
-
     private static final String LAMBDA_METAFACTORY = "java/lang/invoke/LambdaMetafactory";
     private static final Pattern LAMBDA_IMPL_METHOD = Pattern.compile("^lambda\\$\\d+$");
 
     public static byte[] transform(byte[] bytecode, int targetVersion) {
-        asmJava8SupportWorkaround(bytecode);
         resetLambdaClassSequenceNumber();
         ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
         new ClassReader(bytecode).accept(new MyClassVisitor(cw, targetVersion), 0);
         return cw.toByteArray();
-    }
-
-    private static void asmJava8SupportWorkaround(byte[] bytecode) {
-        ByteBuffer buffer = ByteBuffer.wrap(bytecode);
-        short majorVersion = buffer.getShort(MAJOR_VERSION_OFFSET);
-
-        if (majorVersion == JAVA_8_BYTECODE_VERSION) {
-            // XXX: ASM doesn't yet support Java 8, so we must fake the data to be from Java 7
-            buffer.putShort(MAJOR_VERSION_OFFSET, (short) (majorVersion - 1));
-            // TODO: once we can remove this workaround, make our ClassVisitor responsible for setting the bytecode version
-
-        } else if (majorVersion > JAVA_8_BYTECODE_VERSION) {
-            throw new IllegalArgumentException("Only Java 8 and lower is supported, but bytecode version was " + majorVersion);
-        }
     }
 
     private static void resetLambdaClassSequenceNumber() {
