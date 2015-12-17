dbe9fa570c56a0dc3c1ccb5376cb01cd503a66d4
Esko Luontola <esko.luontola@gmail.com>
2015-12-17 13:52:20 +0200

    Run the most valuable tests first

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 set -eux
 
-# Test the forking mechanism
-
-mvn clean verify \
-    --errors \
-    -P fork,noDefaultMethods
-mvn clean verify \
-    --errors \
-    -P fork
-
-# The Maven plugin's minimum requirement is Java 6,
-# but then the plugin must force forking the process
-
-JAVA_HOME="$JAVA6_HOME" mvn clean verify \
-    --errors \
-    -P java6
-
 # Run end-to-end tests against all supported Java versions
+
+mvn clean verify \
+    --errors \
+    -P noDefaultMethods
+mvn clean verify \
+    --errors
 
 mvn clean verify \
     --errors \
@@ -33,8 +23,18 @@ mvn clean verify \
     --errors \
     -P java5
 
+# Test the forking mechanism
+
 mvn clean verify \
     --errors \
-    -P noDefaultMethods
+    -P fork,noDefaultMethods
 mvn clean verify \
-    --errors
+    --errors \
+    -P fork
+
+# The Maven plugin's minimum requirement is Java 6,
+# but then the plugin must force forking the process
+
+JAVA_HOME="$JAVA6_HOME" mvn clean verify \
+    --errors \
+    -P java6
